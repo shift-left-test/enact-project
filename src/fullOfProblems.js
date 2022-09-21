dfed1a1a55176fef240f6d78effc54c812add3e0
Sung Gon Kim <sunggon82.kim@lge.com>
2022-09-21 00:57:57 +0000

    Add a faulty source code for static analysis

---
+++ src/fullOfProblems.js
@@ -0,0 +1,10 @@
+// An example source code from the link below.
+// https://eslint.org/docs/latest/user-guide/formatters/#fullofproblemsjs
+
+function addOne(i) {
+    if (i != NaN) {
+        return i ++
+    } else {
+      return
+    }
+};
